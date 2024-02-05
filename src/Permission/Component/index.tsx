260b3f509e668c19e3f66a43449e7ad6e8f8f1f2
raulpesilva <adraulpesilva@gmail.com>
2024-02-05 12:32:22 -0300

    refac

--- src/Permission/Component/index.tsx
+++ src/Permission/Component/index.tsx
@@ -1,10 +1,7 @@
-import { useMemo } from 'react';
-import { Rule, useRules } from '..';
-import { isAllowed } from '../utils';
+import { Rule, useIsAllowed } from '..';
 
 export const Allow = ({ children, when }: { children: React.ReactNode; when: Rule[] }) => {
-  const rules = useRules();
-  const isValid = useMemo(() => isAllowed(when, rules), [when, rules]);
-  if (!isValid) return null;
+  const isAllowed = useIsAllowed(when);
+  if (!isAllowed) return null;
   return <>{children}</>;
 };
